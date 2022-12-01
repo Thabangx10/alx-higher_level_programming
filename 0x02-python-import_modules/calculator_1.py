e0e0767a3b7f97006ff56d9aeff6e96e0e101e86
Thabangx10 <Thabangx10@gmail.com>
2022-12-01 04:52:10 -0800

    1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+def add():
+    """Adding function
+    Arguments:
+            a->
+            b->
+    Return:
+           add(a, b)
+    """
+    return (a + b)
+
+
+def sub():
+    """Subtract function
+    Arguments:
+            a->
+            b->
+    Return:
+          subtract(a - b)
+    """
+    return (a - b)
+
+
+def mul():
+    """Multiplication function
+    Argument:
+           a->
+           b->
+    Return:
+          multiply(a * b)
+    """
+    return(a * b)
+
+
+def div():
+    """Division function
+    Argument:
+           a->
+           b->
+    Return:
+          divide( a / b)
+    """
+    return int(a / b)
