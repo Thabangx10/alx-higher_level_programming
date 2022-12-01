768c195839f286ade0857055a1271fe9c2b80635
Thabangx10 <Thabangx10@gmail.com>
2022-11-30 21:47:09 -0800

    7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = [:3]
-word_last_2 = [-2:]
+first_3 =word[:3]
+last_2 =word[-2:]
 middle_word = [1:-1]
-print("first 3 letters: {}". format(word_first_3))
-print("Last 2 letters: {}". format(word_last_2))
+print("first 3 letters: {}". format(first_3))
+print("Last 2 letters: {}". format(last_2))
 print("Middle word: {}". format(middle_word))
