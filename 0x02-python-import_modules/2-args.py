de282692c6817b351fa0ec822c92d679d12fc703
Thabangx10 <Thabangx10@gmail.com>
2022-12-01 13:51:09 -0800

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+if __name__ == "__main__":
+    """Printing the num of lists as arguments with the use of len(argv)."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:". format(count))
+    """using the range function to iterate through the count function"""
+    for i in range(count):
+        print("{}: {}". format(i + 1 , sys.argv[i + 1}))
