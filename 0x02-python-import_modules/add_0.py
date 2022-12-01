388ba635898292b1b1662a02566ea4aa958d70c9
Thabangx10 <Thabangx10@gmail.com>
2022-12-01 04:28:20 -0800

    0

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """ Addition function
+    Arguments:
+            a-> int()
+            b-> int()
+    Returns:
+           int(a) + int(b)
+    """
+    return (a + b)
