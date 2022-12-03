0eabd0e2143d09540f3c83597aa855ffb550691f
Thabangx10 <Thabangx10@gmail.com>
2022-12-02 23:34:07 -0800

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,4 +3,3 @@ def no_c(my_string):
     """This method returns a 'new_string' through mapping of the old strings charcters"""
     new_string = my_string.translate({ord(letter): None for letter in 'cC'})
     return new_string
-
