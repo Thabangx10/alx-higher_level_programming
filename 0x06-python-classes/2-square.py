b5c1eb65c8f870e4b6777ef4f412ba8c1f9980d3
Thabangx10 <Thabangx10@gmail.com>
2022-12-20 05:39:17 -0800

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,11 +4,11 @@
 class Square:
     """Square class method"""
     def __init__(self, size=0):
-        """size must be an integer, otherwise raise a TypeError exception with the messa           ge size must be an integer"""
+        """size must be an integer, otherwise raise a TypeError exception with the messa           ge size must be an integer
+
+        if size is less than 0, raise a ValueError exception with the message size must            be >=0 """
         if type(size) != int:
             raise TypeError('size must be an integer')
-        
-        """if size is less than 0, raise a ValueError exception with the message size mu           st be >=0 """
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
