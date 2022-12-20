aec2ce653664bb907620476d0b7400e746a7b1b5
Thabangx10 <Thabangx10@gmail.com>
2022-12-20 02:01:09 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""we will be using __init__ method to assign the size value 
+   as an attribute to self"""
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
