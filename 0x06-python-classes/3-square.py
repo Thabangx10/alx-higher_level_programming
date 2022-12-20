8756d94f4e839a7f62863e0f31e56d9bceaab3da
Thabangx10 <Thabangx10@gmail.com>
2022-12-20 05:47:39 -0800

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,19 +6,20 @@ class Square:
     """Square methods"""
     def __init__(self, size=0):
         
-        """size must be an integer, otherwise raise a TypeError exception with the messa           ge size must be an integer"""
+        """size must be an integer, otherwise raise a TypeError exception with the messa           ge size must be an integer
+        
+        if size is less than 0, raise a ValueError exception with the message size must            be >= 0
+
+        Public instance method: def area(self): that returns the current square area"""
 
         if type(size) != int:
             raise TypeError('size must be integer')
-
-        """if size is less than 0, raise a ValueError exception with the message size mu           st be >= 0"""
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
             self.__size = size
 
-    """Public instance method: def area(self): that returns the current square area"""
-
     def area(self):
+         """Public instance method: def area(self): that returns the current square"""
         size = self.__size
         return size * size
