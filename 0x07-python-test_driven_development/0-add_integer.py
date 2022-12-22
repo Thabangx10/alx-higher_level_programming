84bfaafc51d9e21842fbb9d0d2e211f35d21a4b3
Thabangx10 <Thabangx10@gmail.com>
2022-12-22 01:18:56 -0800

    0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+add_integer:
+    Checks if the parameters are(int) datatypes
+    Return a sum
+"""
+def add_integer(a, b=98):
+    """
+    check if data is int.
+    """
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must an integer")
+    else:
+        return a + b
