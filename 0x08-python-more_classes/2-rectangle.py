19d89004194220e3a49d52fccb143997791ccbf4
Thabangx10 <Thabangx10@gmail.com>
2023-01-04 00:28:13 -0800

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -27,6 +27,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be < 0.")
+        self.__width = value
 
     @property
     def height(self):
@@ -36,9 +37,10 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) != 0:
-            raise TypeError("height must an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be < 0")
+        self.__height = value
 
     def area(self):
         """Return the area"""
