1baec4abf417231a320058953db2b1fd66cb9164
Thabangx10 <Thabangx10@gmail.com>
2023-01-05 01:16:08 -0800

    4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,6 +2,8 @@
 """
 print_square prints a square depending on the "size" parameter
 """
+
+
 def print_square(size):
     """
     Prints a square with a size.
@@ -13,7 +15,7 @@ def print_square(size):
     """
     if type(size) != int:
         raise TypeError("size must be an integer")
-    if type(size) == float and sizze < 0:
+    if type(size) == float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
