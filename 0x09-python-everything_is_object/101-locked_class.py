9104b303707d94180af807c563c78e5e6a461a22
Thabangx10 <Thabangx10@gmail.com>
2023-01-05 07:04:02 -0800

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,4 +7,8 @@ class LockedClass:
     except for onrs called 'first_name'.
     """
 
-    ___slots___ = ["first_name"]
+    def __setattr__(self, attribute, value):
+        if attribute == "first_name":
+            self.__dict__[attribute] = value
+        else:
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
