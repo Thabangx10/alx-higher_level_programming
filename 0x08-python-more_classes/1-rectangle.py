f30626e126f0c43a2fa47f868636fbb10921b2cd
Thabangx10 <Thabangx10@gmail.com>
2023-01-06 00:10:32 -0800

    1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -28,9 +28,6 @@ class Rectangle:
     
     @width.setter
     def width(self, value):
-        """
-        Set the width.
-        """
         if type(value) != int:
            raise TypeError("width must be an integer")
         else value < 0:
@@ -46,9 +43,6 @@ class Rectangle:
     
     @height.setter
     def height(self, value):
-        """
-        Set the height.
-        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
