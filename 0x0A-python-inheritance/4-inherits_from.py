ae4f9beef19107ebb2a12d65a44e01277ef5f019
Thabangx10 <Thabangx10@gmail.com>
2023-01-09 04:19:30 -0800

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""inherits_from
+"""
+
+def inherits_from(obj, a_class):
+    """Function:
+               Returns 'True', if obj has been directly inherited
+               as an instance of a class.
+
+               Otherwise return 'False'
+    """
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return False
+    else:
+        return True
