1ba009a9ac5bc06fc821743e0b73863ce5bb6c52
Thabangx10 <Thabangx10@gmail.com>
2023-01-09 04:23:45 -0800

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseGeomatry
+"""
+
+class BaseGeometry:
+    """Function area()
+    """
+
+    def area(self):
+        """Not implemented yet
+        """
+
+        raise Exception("area() is not implemented")
