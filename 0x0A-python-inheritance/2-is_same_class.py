14222e8e4181e877743fa246fc2356a406bca339
Thabangx10 <Thabangx10@gmail.com>
2023-01-09 04:06:51 -0800

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""id_same_class
+"""
+
+def is_same_class(obj, a_class):
+    """Returns 'True' if obj is the same instance else 'False'
+    """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
