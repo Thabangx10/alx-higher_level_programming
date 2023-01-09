f4dff421240f9c4dcbaf6e2107ba5c7b46e50d6e
Thabangx10 <Thabangx10@gmail.com>
2023-01-09 03:58:50 -0800

    1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""MyList
+"""
+
+def MyList(list):
+    """Contains list
+    """
+
+    def print_sorted(self):
+        """Prints the self in sorted format
+        """
+
+        print(sorted(self))
