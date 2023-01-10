32455f08a432861826fc55708994eba6bcf9bbb5
Thabangx10 <Thabangx10@gmail.com>
2023-01-10 05:27:33 -0800

    6

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""JSON file writing function
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writing to the text file using the JSON representation
+    """
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
