7030370fc634b1513824af082d5e174a3745e41d
Thabangx10 <Thabangx10@gmail.com>
2023-01-10 05:31:39 -0800

    6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create object from a JSON file
+"""
+import json
+
+def def load_from_json_file(filename):
+    """Creating a python object
+    """
+    with open(filename) as f:
+        return json.load(f)
