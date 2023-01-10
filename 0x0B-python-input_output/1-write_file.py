78edcc90b6362e275dc284bd83ede2b625ae62f1
Thabangx10 <Thabangx10@gmail.com>
2023-01-10 04:58:44 -0800

    1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,8 +2,9 @@
 """Writing to a file 
 """
 
-def number_of_lines(filename=""):
-    """
+def write_file(filename="", text=""):
+    """Writing a str to UTF8 text file
+
     You must use the with statement
 
     You don’t need to manage file permission exceptions.
@@ -13,11 +14,5 @@ def number_of_lines(filename=""):
     Your function should overwrite the content of the file if it already exists.
     """
 
-    with open(filename, encoding="utf-8") as readFile:
-        lines = 0
-        while True:
-            line = readFile.readline()
-            if not line:
-                break
-            lines += 1
-        return lines
+    with open(filename,"w", encoding="utf-8") as readFile:
+        return readFile.write(text)
