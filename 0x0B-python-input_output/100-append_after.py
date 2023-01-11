1f53ef2bd420ba6b1dd95f9a18526d9892a6a520
Thabangx10 <Thabangx10@gmail.com>
2023-01-11 00:22:17 -0800

    13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Text file insertion  function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a string.
+    """
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
