56d6a834f49c4b87bd9b822c083df469b7fe7e87
Thabangx10 <Thabangx10@gmail.com>
2023-02-08 00:51:48 -0800

    7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -50,10 +50,12 @@ class Rectangle:
 
     def area(self):
         """Returning the area"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Return the perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
