97871371d8d48e2d76cc18992780c5ec34482672
Thabangx10 <Thabangx10@gmail.com>
2023-02-14 02:49:25 -0800

    0x00

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+-- The list should be sorted by the number of records (descending)
+-- Results: score, umber of records for this score with the label number
+SELECT score, COUNT(*) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
